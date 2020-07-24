dcf448f1ff223eed650f60701b091e27bb205e64
Ionut Birlad <birladionut@yahoo.it>
2020-07-24 18:35:59 +0200

    ripulito e riorganizzato il codice

--- app.js
+++ app.js
@@ -1,92 +1,37 @@
+const kParse = (program) => {
+  const optionValues = program.opts()
 
-// methods
-  // --------------example method--------------
-  const calc = (input1, input2) => {
-    return parseInt(input1) + parseInt(input2);
+  const allOptions = Object.keys(optionValues)
+  const selectedOptions = []
+
+  for (var i = 0; i < allOptions.length; i++) {
+    program[allOptions[i]] && selectedOptions.push(allOptions[i])
   }
-  // --------------example method--------------
 
+  selectedOptions.sort()
 
-  // --------------main method two--------------
-  const kParse = (program) => {
+  const result = new Result(optionValues, selectedOptions)
+  return result.createResult()
+}
 
-    let optionValues = program.opts();
+function Result (inputOne, inputTwo) {
+  this.inputs = inputOne
+  this.chiavi = inputTwo
+}
 
-    // data mockup
-      let allOptions = Object.keys(optionValues);
-      let selectedOptions = [];
-
-      // I check wich options are selected
-      for (var i = 0; i < allOptions.length; i++) {
-        program[allOptions[i]] && selectedOptions.push(allOptions[i]);
-      }
-
-      // here I sort the selected options in alphabetical order
-      selectedOptions.sort();
-    // data mockup
-
-    const result = new Result(optionValues, selectedOptions);
-    return result.createResult();
-
-  }
-  // --------------main method two--------------
-// methods
-
-// ----------------------------CONSTRUCTOR RESULTS----------------------------
-  function Result(inputOne, inputTwo) {
-    this.inputs = inputOne;
-    this.chiavi = inputTwo;
-  }
-// ----------------------------CONSTRUCTOR RESULTS----------------------------
-
-// ------------------------------RESULTS METHODS------------------------------
-  Result.prototype.createResult = function () {
-    let res = {};
-    for (var i = 0; i < this.chiavi.length; i++) {
-      if (this.chiavi[i] == 'bar' && this.inputs[this.chiavi[i]].length > 3) {
-        let spliced = this.inputs[this.chiavi[i]].split(" ");
-        this.inputs[this.chiavi[i]] = spliced;
-      }
-
-      this.chiavi[i] == 'number' ? res[this.chiavi[i]] = parseInt(this.inputs[this.chiavi[i]]) : res[this.chiavi[i]] = this.inputs[this.chiavi[i]];
+Result.prototype.createResult = function () {
+  const res = {}
+  for (var i = 0; i < this.chiavi.length; i++) {
+    if (this.chiavi[i] === 'bar' && this.inputs[this.chiavi[i]].length > 3) {
+      const spliced = this.inputs[this.chiavi[i]].split(' ')
+      this.inputs[this.chiavi[i]] = spliced
     }
-    return res;
-  };
-// ------------------------------RESULTS METHODS------------------------------
 
-// here I export the methods that are used in commander.js
-  module.exports = {
-    calc,
-    kParse
-  };
+    this.chiavi[i] === 'number' ? res[this.chiavi[i]] = parseInt(this.inputs[this.chiavi[i]]) : res[this.chiavi[i]] = this.inputs[this.chiavi[i]]
+  }
+  return res
+}
 
-
-
-
-// ------------------------------discarded version------------------------------
-
-  // --------------main method one--------------
-  // const kParse = (input, inputTwo, inputThree) => {
-  //
-  //   if ((input) && !(inputTwo, inputThree)) {
-  //     return {
-  //       foo: input
-  //     };
-  //   }
-  //
-  //   if ((inputTwo) && !(input, inputThree)) {
-  //     return {
-  //       number: inputTwo
-  //     };
-  //   }
-  //
-  //   if (input, inputTwo, inputThree) {
-  //     return {
-  //       bar: inputThree,
-  //       foo: input,
-  //       number: inputTwo
-  //     };
-  //   }
-  //
-  // }
-  // --------------main method one--------------
+module.exports = {
+  kParse
+}
--- commands.js
+++ commands.js
@@ -1,63 +1,19 @@
 #! /usr/bin/env node
 
-// exporting the global object used by commander
-const { program } = require('commander');
+const { program } = require('commander')
 
-// the required file for the exported methods
-  const {
-    calc,
-    kParse
-  } = require('./app.js');
+const {
+  kParse
+} = require('./app.js')
 
-
-// commands
-// -----------------------------------EXAMPLE-----------------------------------
-  program
-  .command('run <input1> <input2>')
-  .alias('r')
-  .description('This command will take your input and will produce a valid output according to the original kata documentation (see the read.me file)')
-  .action((input1, input2) => {
-    let result = calc(input1, input2);
-    console.log(`Il tuo risultato è ${result}`);
-  });
-  // ----------------------------------EXAMPLE----------------------------------
-
-  // -------------------------arguments parser commands-------------------------
-  program
+program
   .option('-f, --foo [item]', 'option')
   .option('-n, --number <item>', 'option')
   .option('-b, --bar <item>', 'option')
   .description('This command will take your input and will produce a valid output according to the original kata documentation (see the readme.md file)')
   .action(() => {
-    let result = kParse(program);
-    console.log(result);
-  });
-  // -------------------------arguments parser commands-------------------------
-// commands
+    const result = kParse(program)
+    console.log(result)
+  })
 
-// processing data
-program.parse(process.argv);
-
-
-
-
-
-
-
-
-// ------------------------------discarded version------------------------------
-
-// if ((program.foo) && !(program.number, program.bar)) {
-//   let result = kParse(program.foo);
-//   console.log(result);
-// }
-//
-// if ((program.number) && !(program.foo, program.bar)) {
-//   let result = kParse(program.foo, parseInt(program.number));
-//   console.log(result);
-// }
-//
-// if (program.foo, program.number, program.bar) {
-//   let result = kParse(program.foo, parseInt(program.number), program.bar);
-//   console.log(result);
-// }
+program.parse(process.argv)
